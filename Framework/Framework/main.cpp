ba6c9f2d6420a5a0ae1c0a5127306e2db87b2f45
saito.takumi <saito.takumi@appci.jp>
2023-09-26 10:08:26 +0900

    initial commit

---
+++ Framework/Framework/main.cpp
@@ -0,0 +1,21 @@
+//
+//
+//
+//
+
+
+//
+// Include
+//
+#include "App.h"
+
+//
+// メインエントリーポイントです.
+//
+int wmain(int argc, wchar_t** argv, wchar_t** envp)
+{
+	App app(960, 540);
+	app.Run();
+
+	return 0;
+}
